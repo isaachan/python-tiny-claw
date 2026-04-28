c3da0086ba18d4dd70070321803b44220f9aec4d
isaachan <isaachanstar@gmail.com>
2026-04-28 10:54:37 +0800

    start implementing openai provider

--- internal/provider/llmprovider.py
+++ internal/provider/llmprovider.py
@@ -1,9 +1,9 @@
 from context.context import Context
-from schema.message import Message
+from schema.message import Message, ToolDefinition
 
 class LLMProvider:
 
     #TODO type of available_tools
-    def generate(self, ctx: Context, context_history: list[Message], available_tools) -> Message: 
+    def generate(self, ctx: Context, context_history: list[Message], available_tools: list[ToolDefinition]) -> Message: 
 
         return None # type: ignore
